d947db1217ee2a35e752a8331f16a6047b846709
huangxing <huangxing@arhieason.com>
2017-08-01 17:12:58 +0800

    日志模块完成

--- python-framework/__init__.py
+++ python-framework/__init__.py
@@ -5,4 +5,10 @@
 # @date:17-8-1
 
 
+import os
+import sys
 
+file_path = os.path.realpath(__file__)
+project_dir = os.path.dirname(file_path)
+os.chdir(project_dir)
+sys.path.append(project_dir)
--- python-framework/manage.py
+++ python-framework/manage.py
@@ -4,5 +4,16 @@
 # @author: x.huang
 # @date:17-8-1
 
+import os
+import sys
 
+from libs.loger import root_logger as logger
 
+# 初始化环境
+file_path = os.path.realpath(__file__)
+project_dir = os.path.dirname(file_path)
+os.chdir(project_dir)
+sys.path.append(project_dir)
+
+# start code
+logger.warning('waring')
